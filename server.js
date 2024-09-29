6ad1803afbdb931b4ef0ac11d93b068be5bf1ba4
Alan Vasconcelos <alandbh@gmail.com>
2024-09-29 02:05:11 -0300

    Turning back to render server

--- server.js
+++ server.js
@@ -16,18 +16,18 @@ function getExt(filename) {
     return filename.split(".").pop();
 }
 
-// // Caminho absoluto para a pasta de uploads
-// const uploadFolder = path.join(__dirname, "/tmp");
+// Caminho absoluto para a pasta de uploads
+const uploadFolder = path.join(__dirname, "/tmp");
 
-// // Verifica se a pasta 'uploads' existe; se não, cria a pasta
-// if (!fs.existsSync(uploadFolder)) {
-//     fs.mkdirSync(uploadFolder, { recursive: true });
-// }
+// Verifica se a pasta 'uploads' existe; se não, cria a pasta
+if (!fs.existsSync(uploadFolder)) {
+    fs.mkdirSync(uploadFolder, { recursive: true });
+}
 
-// // Configura o multer para armazenar arquivos nessa pasta
+// Configura o multer para armazenar arquivos nessa pasta
 // const upload = multer({ dest: uploadFolder });
 
-const uploadFolder = "/tmp"; // Diretório temporário correto no App Engine
+// const uploadFolder = "/tmp"; // Diretório temporário correto no App Engine
 
 // Verifica se a pasta 'tmp' existe; se não, cria a pasta
 if (!fs.existsSync(uploadFolder)) {
@@ -41,10 +41,10 @@ const upload = multer({
 });
 
 // Ativa o CORS para uma origem específica ou todas
-const corsOptions = {
-    origin: "http://localhost:3003", // ou '*' para permitir todas as origens
-    credentials: true, // se estiver enviando cookies ou autenticação
-};
+// const corsOptions = {
+//     origin: "http://localhost:3003", // ou '*' para permitir todas as origens
+//     credentials: true, // se estiver enviando cookies ou autenticação
+// };
 
 // Inicializa o servidor Express
 const app = express();
@@ -54,7 +54,7 @@ app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ limit: "500mb", extended: true }));
 
 // Ativa o CORS para todas as origens
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.post("/upload", upload.single("file"), async (req, res) => {
     const filePath = path.join(uploadFolder, req.file.filename);
